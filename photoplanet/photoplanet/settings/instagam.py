d710d494a9499223f871dedad0be65b41cce870e
rewkha <rewkha.dev@gmail.com>
2013-07-05 18:37:22 +0300

    Delete instagam.py

--- photoplanet/photoplanet/settings/instagam.py
+++
@@ -1,5 +0,0 @@
-# rename to instagram.py
-# get variables from http://instagram.com/developer/clients/register/
-
-INSTAGRAM_CLIENT_ID = '87a6eb3c66da4a2db689921f96b5eb48'
-INSTAGRAM_CLIENT_SECRET = 'b27c2e5e99644be5a1f94720bab84d79'

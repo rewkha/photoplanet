4c5c1abb0a629297a9742952bfe7792e439aaf5c
AndrewK <rew.kha@gmail.com>
2013-07-06 19:57:55 +0300

    add secret keys

---
+++ photoplanet/photoplanet/settings/local.py
@@ -0,0 +1,6 @@
+from base import *
+from instagram import *
+#from google import *
+
+INSTAGRAM_CLIENT_ID = '87a6eb3c66da4a2db689921f96b5eb48'
+INSTAGRAM_CLIENT_SECRET = 'b27c2e5e99644be5a1f94720bab84d79'

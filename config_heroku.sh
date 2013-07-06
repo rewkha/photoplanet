3cf01588a5372d05c4374d39af41ed9c50a75c92
AndrewK <rew.kha@gmail.com>
2013-07-06 19:32:42 +0300

    add heroku config

--- config_heroku.sh
+++ config_heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 heroku config:set DJANGO_SETTINGS_MODULE=photoplanet.settings.heroku
-heroku config:set INSTAGRAM_CLIENT_ID=YOUR_INSTAGRAM_CLIENT_ID
-heroku config:set INSTAGRAM_CLIENT_SECRET=YOUR_INSTAGRAM_CLIENT_SECRET
+heroku config:set INSTAGRAM_CLIENT_ID=87a6eb3c66da4a2db689921f96b5eb48
+heroku config:set INSTAGRAM_CLIENT_SECRET=b27c2e5e99644be5a1f94720bab84d79
 heroku config:set WEBSITE URL=http://rewkha-planet.herokuapp.com/

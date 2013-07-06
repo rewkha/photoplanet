fb4c0f8a910c29739cefd3812beea7a09335d927
AndrewK <rew.kha@gmail.com>
2013-07-07 01:03:29 +0300

    fx in heroku deploy

--- config_heroku.sh
+++ config_heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 heroku config:set DJANGO_SETTINGS_MODULE=photoplanet.settings.heroku
-heroku config:set INSTAGRAM_CLIENT_ID=87a6eb3c66da4a2db689921f96b5eb48
-heroku config:set INSTAGRAM_CLIENT_SECRET=b27c2e5e99644be5a1f94720bab84d79
-heroku config:set WEBSITE URL=http://rewkha-planet.herokuapp.com/
+heroku config:set INSTAGRAM_CLIENT_ID=90ab5f87d9814062965778036e2e4572
+heroku config:set INSTAGRAM_CLIENT_SECRET=5f24034ea61a4bddb0f10137b31e9793
+heroku config:set WEBSITE URL=http://infinite-gorge-6776.herokuapp.com/

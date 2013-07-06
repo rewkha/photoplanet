a43919cbd70b34384d528b3c8b4e52b916e63d0c
AndrewK <rew.kha@gmail.com>
2013-07-07 01:11:43 +0300

    add to gitignore

--- config_heroku.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-heroku config:set DJANGO_SETTINGS_MODULE=photoplanet.settings.heroku
-heroku config:set INSTAGRAM_CLIENT_ID=90ab5f87d9814062965778036e2e4572
-heroku config:set INSTAGRAM_CLIENT_SECRET=5f24034ea61a4bddb0f10137b31e9793
-heroku config:set WEBSITE URL=http://infinite-gorge-6776.herokuapp.com/
